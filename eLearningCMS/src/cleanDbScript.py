eb71639797cfb6b2395175495d22f03ecf4bf254
Kunal Agrawal <kunala@internal.synopsys.com>
2018-05-05 13:45:30 +0530

    updating cleanDbScript

--- eLearningCMS/src/cleanDbScript.py
+++ eLearningCMS/src/cleanDbScript.py
@@ -1,6 +1,10 @@
 import os
 import shutil
-folders = ['__pycache__','migrations']
 for root, dirs, files in os.walk("."):
-	if os.path.basename(root) in folders:
+	if os.path.basename(root) == 'migrations':
+		for file in files:
+			path = os.path.abspath(os.path.join(root, file))
+			if (os.path.basename(path) != '__init__.py'):
+				os.remove(path)
+	elif os.path.basename(root) == '__pycache__':
 		shutil.rmtree(root)
