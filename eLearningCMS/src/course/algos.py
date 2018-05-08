82f9294d3617adaabd8c6fc25b8399b2c93fed79
Kushal Ghosh <Kushal_Ghosh@symantec.com>
2018-05-09 00:51:35 +0530

    Adding some course related algos

---
+++ eLearningCMS/src/course/algos.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from . import models
+import re
+from collections import OrderedDict
+from operator import itemgetter
+
+# returns the 'count' courses which have maximum students
+def mostEnrolledCourses(count):
+    return models.Course.objects.raw('SELECT * FROM course_course WHERE id IN (SELECT course_id FROM course_enrolledcourse GROUP BY course_id ORDER BY COUNT(course_id) DESC LIMIT ' + str(count) + ')')
+
+# return all courses which have 'searchtext' delimited by ' ,'
+def searchCourses(searchtext, provider = None, exam = None):
+    tokens = re.split(' |,', searchtext)
+    coursesDict = {}
+
+    query = 'SELECT * from course_course WHERE published = 1'
+    if provider is not None:
+        query = query + ' AND provider_id = ' + str(provider)
+    if exam is not None:
+        query = query + ' AND exam = \'' + exam + '\''
+
+    for text in tokens:
+            if text == '':
+                continue
+            courses = models.Course.objects.raw(query + ' AND name LIKE \'%' + text + '%\'')
+
+            for c in courses:
+                if c in coursesDict.keys():
+                    coursesDict[c] = coursesDict[c] + 1
+                else:
+                    coursesDict[c] = 1
+
+    courseList = OrderedDict(sorted(coursesDict.items(), key = itemgetter(1)))
+    return courseList
+
+# return all published courses
+def getPublishedCourses():
+    return models.Course.objects.filter(published=1)
+
+#returns exam list
+def getExams():
+    return models.Course.objects.raw('SELECT * from course_course GROUP BY (exam)')
