2e7934d630091c830b32aca3ab0d98ef037737a8
Kunal Agrawal <kunala@internal.synopsys.com>
2018-05-12 16:03:56 +0530

    Adding structure of Home page

--- eLearningCMS/src/examcracker/views.py
+++ eLearningCMS/src/examcracker/views.py
@@ -1,8 +1,18 @@
 from django.views import generic
-
+from course.models import Course
+from provider.models import Provider
+from course.algos import *
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        examsList = getExams()
+        courseList = getPublishedCourses()
+        kwargs["exams"] = examsList
+        kwargs["allCourses"] = courseList
+        return super().get(request, *args, **kwargs)
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
