8d8b1a922c1e3e5018c392268708fb258405dc96
Kushal Ghosh <Kushal_Ghosh@symantec.com>
2018-05-23 01:50:12 +0530

    Adding support for Shopping cart

--- eLearningCMS/src/payments/urls.py
+++ eLearningCMS/src/payments/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^process/$', views.payment_process, name='process'),
     url(r'^done/$', views.payment_done, name='done'),
     url(r'^canceled/$', views.payment_done, name='canceled'),
+    path('cart', views.Cart.as_view(), name='my_cart')
 ]
