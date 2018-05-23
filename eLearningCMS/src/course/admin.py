52933b192d589782830ac95a1ed6f2b56ef2d8e2
Kunal Agrawal <kunala@internal.synopsys.com>
2018-05-24 00:02:28 +0530

    Adding edit/create new course creation flow --> Adding course description part

--- eLearningCMS/src/course/admin.py
+++ eLearningCMS/src/course/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Course)
+admin.site.register(models.CourseChapter)
+admin.site.register(models.CoursePattern)
 # Register your models here.
