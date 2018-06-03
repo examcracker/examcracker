0fbccbab8b087d106dc54216b43f3d9ea1282f9a
Kushal Ghosh <Kushal_Ghosh@symantec.com>
2018-06-03 23:28:14 +0530

    Silly mistake

--- eLearningCMS/src/payments/urls.py
+++ eLearningCMS/src/payments/urls.py
@@ -7,6 +7,6 @@ app_name = 'payments'
 urlpatterns = [
     url(r'^process/$', views.payment_process, name='process'),
     url(r'^done/$', views.payment_done, name='done'),
-    url(r'^canceled/$', views.payment_done, name='canceled'),
+    url(r'^canceled/$', views.payment_canceled, name='canceled'),
     path('cart', views.Cart.as_view(), name='my_cart')
 ]
