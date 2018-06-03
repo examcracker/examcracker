56262262bbde433a241428251f68674408ee3b84
Kushal Ghosh <Kushal_Ghosh@symantec.com>
2018-06-03 22:58:30 +0530

    Registering models with admin

--- eLearningCMS/src/course/admin.py
+++ eLearningCMS/src/course/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 admin.site.register(models.Course)
 admin.site.register(models.CourseChapter)
+admin.site.register(models.EnrolledCourse)
--- eLearningCMS/src/provider/admin.py
+++ eLearningCMS/src/provider/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
 from . import models
 
+# Register your models here.
 admin.site.register(models.Session)
 admin.site.register(models.Provider)
