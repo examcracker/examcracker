b83f2ad12e00c6bcb554c761d080a56f1d4efe08
Kushal Ghosh <Kushal_Ghosh@symantec.com>
2018-07-31 09:55:06 +0530

    Adding notification for email verification

--- eLearningCMS/src/profiles/signals.py
+++ eLearningCMS/src/profiles/signals.py
@@ -5,6 +5,7 @@ import logging
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+import notification
 from . import models
 
 logger = logging.getLogger("project")
@@ -44,5 +45,6 @@ def create_profile_handler(sender, instance, created, **kwargs):
     if instance.is_staff:
         typeofuser = 'provider'
 
+    notification.models.notify(instance.id, notification.models.EMAIL_NOT_VERIFIED, notification.models.WARNING, instance.email)
     sendVerificationMail(instance.email, typeofuser, profile.slug)
     logger.info('New user profile for {} created'.format(instance))
