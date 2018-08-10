e8dffe58f2b5ca73605aefda530a451f32a30ba0
Kunal Agrawal <kunala@internal.synopsys.com>
2018-08-10 21:26:29 +0530

    Removing ffmpy import.

--- eLearningCMS/src/provider/models.py
+++ eLearningCMS/src/provider/models.py
@@ -6,7 +6,7 @@ from django.core.files.storage import FileSystemStorage
 from django.template.defaultfilters import filesizeformat
 from django.utils.translation import ugettext_lazy as _
 import subprocess
-import ffmpy
+#import ffmpy
 import json
 import os
 
