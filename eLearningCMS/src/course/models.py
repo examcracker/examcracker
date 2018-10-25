0f9cc45a2967baf0412d4606b5a0b7bf09cf2ec6
Kushal Ghosh <kghoshnitk@gmail.com>
2018-10-25 07:42:28 +0530

    Schema for schedule

--- eLearningCMS/src/course/models.py
+++ eLearningCMS/src/course/models.py
@@ -122,4 +122,10 @@ class CourseReview(models.Model):
     rating = models.IntegerField(default=0)
     reviewed = models.DateTimeField(auto_now_add=True)
 
+class Schedule(models.Model):
+    provider = models.ForeignKey(provider.models.Provider, on_delete=models.CASCADE)
+    chapter = models.ForeignKey(CourseChapter, on_delete=models.CASCADE)
+    start = models.DateTimeField(auto_now_add=True)
+    duration = models.IntegerField(default=60) #minutes
+    recurafter = models.IntegerField(default=0) #0 hours => does not recer
 
