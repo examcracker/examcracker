4931525de0d0f0cb8b276380543cad67435c7001
Hemant <heman_t019@yahoo.co.in>
2018-11-19 22:00:04 +0530

    Merge the client code

--- client/service.py
+++ client/service.py
@@ -1,36 +1,39 @@
+from __future__ import print_function
+import captureFeed
+import uploadVideo
+import configparser
+import os
 import websocket
 import time
 import api
 import json
-import capture
 
 def on_message(ws, message):
-    responseDict = {}
-    messageDict = json.loads(message)
+	responseDict = {}
+	messageDict = json.loads(message)
 
-    if "command" in messageDict.keys():
-        command = messageDict["command"]
-        if command == api.command_start:
-            if ws.obj.capturing:
-                responseDict["result"] = api.status_capture_started
-            else:
-                chapterid = messageDict["chapterid"]
-                responseDict["result"] = api.status_success
-                ws.obj.capturing = True
-                ws.obj.chapterid = chapterid
-                capture.startCapturing(ws)
-        elif command == api.command_stop:
-            if not ws.obj.capturing:
-                responseDict["result"] = api.status_no_capture_started
-                responseDict["result"] = api.status_success
-            else:
-                cature.stopCapturing(ws, ws.obj.videokey, ws.obj.chapterid)
-                ws.obj.capturing = False
+	if "command" in messageDict.keys():
+		command = messageDict["command"]
+		if command == api.command_start:
+			if ws.obj.capturing:
+				responseDict["result"] = api.status_capture_started
+			else:
+				chapterid = messageDict["chapterid"]
+				responseDict["result"] = api.status_success
+				ws.obj.chapterid = chapterid
+				ws.obj.startCapture()
+				
+		elif command == api.command_stop:
+			if not ws.obj.capturing:
+				responseDict["result"] = api.status_no_capture_started
+			else:
+				ws.obj.stopCapture()
+				responseDict["result"] = api.status_success
 
-        ws.send(json.dumps(responseDict))
-    else if "url" in messageDict.keys():
-        videokey = messageDict["url"]
-        ws.obj.videokey = videokey
+		ws.send(json.dumps(responseDict))
+	else:
+		print ("Unhandled command: ", messageDict.keys())
+
 
 def on_error(ws, error):
     print(error)
@@ -39,21 +42,54 @@ def on_close(ws):
     pass
 
 class ClientService(object):
-    wsclient = None
-    capturing = False
-    chapterid = None
-    videokey = None
+	def __init__(self):
+		wsclient = None
+		capturing = False
+		chapterid = None
+		videokey = None
+		self.wsclient = None
+		websocket.enableTrace(True)
 
-    def __init__(self):
-        self.wsclient = None
-        websocket.enableTrace(True)
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		config = configparser.ConfigParser()
+		config.readfp(open(os.path.join(dir_path, 'arguments.cfg')))
 
-    def close(self):
-        self.wsclient.close()
+		self.clientId = config.get("config","clientId")
 
-    def run(self):
-        self.wsclient = websocket.WebSocketApp("ws://127.0.0.1:8000/ws/gyaan/", on_message = on_message, on_close = on_close,
-                                          on_error = on_error)
-        self.wsclient.obj = self
-        self.wsclient.run_forever()
+		self.capture = captureFeed.captureFeed(self.clientId)
+		self.upload = uploadVideo.uploadVideo(self.clientId)
+		self.wsClient = websocketClient.clientWebsocket(self.clientId)
+
+	def close(self):
+		self.wsclient.close()
+		
+	def startCapture(self):
+		if self.capturing:
+			print ("Already capturing")
+			return
+			
+		self.capturing = True
+		self.capture.startCapturing()
+
+	def stopCapture(self):
+		if not self.capturing:
+			print ("No active capturing")
+			return
+			
+		self.capturing = False
+		self.capture.stopCapturing()
+		#uploadResponse = self.upload.uploadVideoJW(self.capture.outputFileName)
+
+
+	def run(self):
+		self.wsclient = websocket.WebSocketApp("ws://127.0.0.1:8000/ws/gyaan/", on_message = on_message, on_close = on_close, on_error = on_error)
+		self.wsclient.obj = self
+		self.wsclient.run_forever()
+		
+def main():
+	service = ClientService()
+	service.run()
+
+if __name__ == "__main__":
+	main()
 
