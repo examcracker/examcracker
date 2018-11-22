bce779f6298732cd72e5fff8a57347e685c90638
Kushal Ghosh <kghoshnitk@gmail.com>
2018-11-22 10:54:16 +0530

    Adding code to store encrypted provider id in db

--- eLearningCMS/src/provider/models.py
+++ eLearningCMS/src/provider/models.py
@@ -5,13 +5,25 @@ from django.forms import forms
 from django.core.files.storage import FileSystemStorage
 from django.template.defaultfilters import filesizeformat
 from django.utils.translation import ugettext_lazy as _
+from Crypto.Cipher import AES
 import subprocess
 import json
 import os
+import base64
+
+aes_key = base64.b64decode("iUmAAGnhWZZ75Nq38hG76w==")
+aes_iv = base64.b64decode("rgMzT3a413fIAvESuQjt1Q==")
 
 class Provider(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     approved = models.BooleanField(default=False)
+    encryptedid = models.CharField(default='', max_length=30)
+
+    def save(self):
+        super().save()
+        cipher = AES.new(aes_key, AES.MODE_CFB, aes_iv)
+        self.encryptedid = base64.b64encode(cipher.encrypt(str(self.id).encode())).decode()
+        super().save()
 
 def user_directory_path(instance, filename):
     return 'sessions/{0}/{1}'.format(instance.provider.id, filename)
