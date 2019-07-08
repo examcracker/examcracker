1d0fd69cea13bb2f9822cdd8437b503eed2f2ced
kunal <kunaldceit@gmail.com>
2019-07-08 23:12:28 +0530

    Fixing this function as well.

--- eLearningCMS/src/schedule/views.py
+++ eLearningCMS/src/schedule/views.py
@@ -138,6 +138,8 @@ def getActiveSchedules(providerId):
 
 def isAnyEventLive(request):
     scheduleObj = models.Schedule.objects.filter(running=1)
+    if not scheduleObj:
+        return False
     if request.user.is_staff:
         # Get Live events of Scheduled courses
         providerObj = getProvider(request)
@@ -145,9 +147,19 @@ def isAnyEventLive(request):
     else:
         # Get live events of enrolled courses
         studentObj = student.models.Student.objects.filter(user_id=request.user.id)[0]
-        enrolledCourseObj = course.models.EnrolledCourse.objects.filter(student_id=studentObj.id).values('course_id')
-        courseChapterObj = course.models.CourseChapter.objects.filter(Q(course_id__in=enrolledCourseObj))
-        scheduleObj = scheduleObj.filter(Q(chapter_id__in=courseChapterObj))
+        enrolledCourseObj = course.models.EnrolledCourse.objects.filter(student_id=studentObj.id)
+        chaptersList = []
+        for ec in enrolledCourseObj:
+            if ec.chapteraccess == '':
+                courseChapterObj = course.models.CourseChapter.objects.filter(course_id=ec.course_id)
+                for cco in courseChapterObj:
+                    chaptersList.append(cco.id)
+            else:
+                allowedModules = ec.chapteraccess.split(',')
+                allowedModulesInt = [int(i) for i in allowedModules]
+                chaptersList.extend(allowedModulesInt)
+            #courseChapterObj = course.models.CourseChapter.objects.filter(Q(course_id__in=enrolledCourseObj.values('course_id'))).values('id')
+        scheduleObj = scheduleObj.filter(Q(chapter_id__in=chaptersList))
 
     if scheduleObj :
         return True
